138dd84a01012df158bdb29d525a7c2361d16896
kunikatsu hatano <>
2017-11-30 19:39:42 +0900

    reservations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_many :photos
+  has_many :reservations
     #必須項目
     validates :area_type, presence: true
     validates :category_type, presence: true
